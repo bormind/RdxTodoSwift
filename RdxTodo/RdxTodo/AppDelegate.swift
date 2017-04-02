105278f662cf081f6527444611d160eb36036a9f
Boris Schneiderman <you@example.com>
2017-04-02 12:47:56 -0700

    Removed sorryboard

--- RdxTodo/RdxTodo/AppDelegate.swift
+++ RdxTodo/RdxTodo/AppDelegate.swift
@@ -13,9 +13,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        self.window = UIWindow(frame: UIScreen.main.bounds)
+
+        let mainViewController = MainViewController()
+        let nc = UINavigationController(rootViewController: mainViewController)
+        self.window!.rootViewController = nc
+        self.window!.makeKeyAndVisible()
+
         return true
     }
 
